d7a57d1d68cc16484d951e7c1e5f3f4d4f8df8f4
joy0316webiee <joy0316webiee@gmail.com>
2019-05-13 03:10:03 +1000

    register page ui & register with firebase auth

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,12 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Login extends Component {
   render() {
-    return (
-      <div>
-        Login
-      </div>
-    )
+    return <div>Login</div>;
   }
 }
 
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,12 +1,109 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import firebase from "../../firebase";
+
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from "semantic-ui-react";
 
 class Register extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: ""
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(createdUser => {
+        console.log(createdUser);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
   render() {
+    const { username, email, password, passwordConfirmation } = this.state;
+
     return (
-      <div>
-        Register
-      </div>
-    )
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for DevChat
+          </Header>
+          <Form onSubmit={this.handleSubmit} size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                type="text"
+                name="username"
+                value={username}
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+              />
+
+              <Form.Input
+                fluid
+                type="email"
+                name="email"
+                value={email}
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email Address"
+                onChange={this.handleChange}
+              />
+              <Form.Input
+                fluid
+                type="password"
+                name="password"
+                value={password}
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChange}
+              />
+
+              <Form.Input
+                fluid
+                type="password"
+                name="passwordConfirmation"
+                value={passwordConfirmation}
+                icon="repeat"
+                iconPosition="left"
+                placeholder="Password Confirmation"
+                onChange={this.handleChange}
+              />
+
+              <Button fluid size="large" color="orange">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already a user? <Link to="/login">login</Link>{" "}
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
