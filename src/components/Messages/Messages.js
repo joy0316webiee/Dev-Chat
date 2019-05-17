f516a1e72da6228f88e679ff2c2a21324ab97c30
joy0316webiee <joy0316webiee@gmail.com>
2019-05-17 17:43:31 +1000

    color picker & change app colors

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -3,6 +3,8 @@ import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
 import Message from "./Message";
 import firebase from "../../firebase";
+import { connect } from "react-redux";
+import { setUserPosts } from "../../actions";
 import { Segment, Comment } from "semantic-ui-react";
 
 class Messages extends React.Component {
@@ -60,7 +62,24 @@ class Messages extends React.Component {
         messageLoading: false
       });
       this.countUniqueUsers(loadedMessages);
+      this.countUserPosts(loadedMessages);
     });
+  };
+
+  countUserPosts = messages => {
+    let userPosts = messages.reduce((acc, message) => {
+      if (message.user.name in acc) {
+        acc[message.user.name].count += 1;
+      } else {
+        acc[message.user.name] = {
+          avatar: message.user.avatar,
+          count: 1
+        };
+      }
+      return acc;
+    }, {});
+
+    this.props.setUserPosts(userPosts);
   };
 
   countUniqueUsers = messages => {
@@ -202,4 +221,7 @@ class Messages extends React.Component {
   }
 }
 
-export default Messages;
+export default connect(
+  null,
+  { setUserPosts }
+)(Messages);
